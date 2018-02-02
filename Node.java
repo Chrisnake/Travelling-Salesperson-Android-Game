b11976c3adc88a231cc7bc62fc435f516d788693
Chrisnake <32743122+Chrisnake@users.noreply.github.com>
2018-02-02 23:39:31 +0000

    Update Node.java
    I have finished task 1 from the Scrum Sprint Cycles document and this Node class can now convert the X and Y squared paper format to the screen resolution format. The next task now is to find a way to draw a line between the two nodes. (I have found a link that has the exact answer to this question - https://stackoverflow.com/questions/2902640/android-get-the-screen-resolution-pixels-as-integer-values).

--- Node.java
+++ Node.java
@@ -1,13 +1,49 @@
+import java.util.ArrayList;
 
 public class Node extends Object
 {
-	public int nodenumber, x,y;
+	int levelid,nodenumber;
+	double x, y;
+	int ScreenX = 1280; //Example X - In Android Studios use function to get screen X
+	int ScreenY = 720;  //Example Y - In Android Studios use function to get screen Y
+
 	
-	Node(int pp, int xx,int yy) 
+	Node(int lid, double xx, double yy, int pp) 
 	{
+		levelid = lid; //levelid is the level id and is a global variable
+		x = ConvertX(xx);
+		y = ConvertY(yy);
 		nodenumber = pp; //nodenumber is the number of the node. For example node A = 1.
-		x = xx; //x is the x coordinates (not translated) of the node
-		y = yy; //y is the y coodinates (not translated) of the node
+	}
+	
+	public void setX(double xx)
+	{
+		x = xx;
+	}
+	
+	public void setY(double yy)
+	{
+		y = yy;
+	}
+	
+	public double ConvertX(double x) //Converting X from normal squared paper measurements to on screen measurements.
+	{
+		double newX;
+		newX = ScreenX / 20; //We are dividing by 20 here because this is the MAX X measurements of the squared paper
+		x = newX * x;
+		setX(x); 
+		System.out.print(" x coordinate " + x);
+		return x;
+	}
+	
+	public double ConvertY(double y) //Converting Y from normal squared paper measurements to on screen measurements.
+	{
+		double newY;
+		newY = ScreenY / 60; //We are dividing by 20 here because this is the MAX Y measurements of the squared paper
+		y = newY * y;
+		setY(y);
+		System.out.println(" y coordinate " + y);
+		return y;
 	}
 	
 	public void Print() //This method prints out the node position
