fa6fcc6d0f865b535b2bd913b77563c7b207981c
Christian <1605942@brunel.ac.uk>
2018-02-12 21:03:22 +0000

    I have added the level 1 activity including the nodes, however edges still need to be implemented.

--- Android App/app/src/main/java/com/example/android/graphgame/ELV1Activity.java
+++ Android App/app/src/main/java/com/example/android/graphgame/ELV1Activity.java
@@ -2,9 +2,15 @@ package com.example.android.graphgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class ELV1Activity extends AppCompatActivity
 {
+    static int levelid = 1;
 //TODO: Research how to input our nodes/dots onto the phone screen and keep the nodes/dots at the same position even when the screen resolution changes
 //TODO: Find a way to input the nodes and allow the user to click on a node to draw a line.
 //TODO: After the user clicks on a node and a new line is drawn, add the weight onto the total weight to show the users score
@@ -17,6 +23,50 @@ public class ELV1Activity extends AppCompatActivity
         setContentView(R.layout.activity_elv1);
     }
 
-    //Create the nodes from buttons or image buttons to allow on click listener.
+    public static void main(String args[])
+    {
+        ArrayList<Levels> Level1 = new ArrayList<Levels>(); //Creating the matrix for level1
+        ArrayList <Node> L1N = new ArrayList <Node>();
+
+        int g[][] = {{0,1,2},
+                     {1,0,3},
+                     {2,3,0}};
+        Levels Matrix = new Levels(levelid, g); //I have created a level above in the form of a 2D array and have inputted the information into level 1.
+        Level1.add(Matrix);
+
+        Node node1 = new Node(levelid, 10, 50, 20); //Node 1 at x coordinate 10, and y coordinate 20.
+        Node node2 = new Node(levelid, 5, 25, 40);
+        Node node3 = new Node(levelid, 15, 25, 10);
+
+        L1N.add(node1);
+        L1N.add(node2);
+        L1N.add(node3);
+    }
+
+    //TODO: We need to learn how to use ontouch listeners and which classes to put them in. This is for when the user touches the screen and it automatically goes to the nearest node
+    private View.OnTouchListener handleTouch = new View.OnTouchListener()
+    {
+        @Override
+        public boolean onTouch(View v, MotionEvent event)
+        {
+            int x = (int) event.getX(); //The x coordinate of where the user touches the screen
+            int y = (int) event.getY(); //The y coordinate of where the user touches the screen
+
+            switch (event.getAction())
+            {
+                case MotionEvent.ACTION_DOWN:
+                    Log.i("TAG", "touched down");
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    Log.i("TAG", "moving: (" + x + ", " + y + ")");
+                    break;
+                case MotionEvent.ACTION_UP:
+                    Log.i("TAG", "touched up");
+                    break;
+            }
+            return true;
+        }
+    };
+
 
 }
